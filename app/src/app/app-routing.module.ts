91a4ed35d490d672f15cb5346bd77acd2deb8f3b
vignesh96 <vigkad9600@gmail.com>
2020-03-22 14:08:41 +0530

    Added login and register component. Initial routing changes.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
+import { RegisterationComponent } from './components/registeration/registeration.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'register', component: RegisterationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterationComponent } from './components/registeration/registeration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterationComponent
   ],
   imports: [
     BrowserModule,
