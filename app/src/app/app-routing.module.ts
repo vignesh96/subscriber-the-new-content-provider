1574f0d288407487b16e8093a2c727b51bf02b3d
vignesh96 <vigkad9600@gmail.com>
2020-03-22 16:44:54 +0530

    Provided routing for register page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { RegisterationComponent } from './components/registeration/registeration.component'
+import { HomeComponent } from './components/home/home.component'
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path : 'register', component: RegisterationComponent}
 ];
 
