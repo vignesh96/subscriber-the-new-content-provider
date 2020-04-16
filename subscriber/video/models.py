3b22b1c5a76f8a0eaac05eb7bf950871d94e6206
vignesh96 <vigkad9600@gmail.com>
2020-04-16 21:59:38 +0530

    video upload form changes

--- subscriber/video/models.py
+++ subscriber/video/models.py
@@ -12,11 +12,10 @@ class Upload(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     s3_url = models.CharField(max_length=100)
     no_of_likes = models.IntegerField()
-    video = S3DirectField()
     content = models.TextField()
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('video-detail', kwargs={'pk': self.pk})
+         return reverse('main-home')
